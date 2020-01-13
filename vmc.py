bd1738bd81520a41f29e7c3fa85c3cc0b7d0323d
Vladimir <Konjkov.VV@gmail.com>
2020-01-13 07:55:27 +0700

    add random

--- vmc.py
+++ vmc.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from random import random
+
 import numpy as np
 import numba as nb
 
@@ -9,13 +11,13 @@ from readers.gwfn import Gwfn
 
 @nb.jit(nopython=True, cache=True)
 def vmc(equlib, stat, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, natom, atomic_positions, atom_charges):
-    dX_max = 0.4
+    dX_max = 0.5
     X = np.random.uniform(-dX_max, dX_max, size=3)
     p = wfn(X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
     for i in range(equlib):
         new_X = X + np.random.uniform(-dX_max, dX_max, size=3)
         new_p = wfn(new_X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if new_p*new_p/p/p > np.random.random_sample(1)[0]:
+        if (new_p/p)**2 > 1 or (new_p/p)**2 < random():
             X, p = new_X, new_p
 
     j = 0
@@ -23,7 +25,7 @@ def vmc(equlib, stat, mo, nshell, shell_types, shell_positions, primitives, cont
     for dX in range(stat):
         new_X = X + np.random.uniform(-dX_max, dX_max, size=3)
         new_p = wfn(new_X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if (new_p/p)**2 > np.random.random_sample(1)[0]:
+        if (new_p/p)**2 > 1 or (new_p/p)**2 < random():
             X, p = new_X, new_p
             j += 1
             sum += local_energy(X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, natom, atomic_positions, atom_charges)
@@ -35,4 +37,4 @@ if __name__ == '__main__':
     gwfn = Gwfn('test/h/HF/cc-pVQZ/gwfn.data')
     mo = gwfn.mo[0, 0]
 
-    print(vmc(5000, 10000000, mo, gwfn.nshell, gwfn.shell_types, gwfn.shell_positions, gwfn.primitives, gwfn.contraction_coefficients, gwfn.exponents, gwfn.natom, gwfn.atomic_positions, gwfn.atom_charges))
+    print(vmc(5000, 10**7, mo, gwfn.nshell, gwfn.shell_types, gwfn.shell_positions, gwfn.primitives, gwfn.contraction_coefficients, gwfn.exponents, gwfn.natom, gwfn.atomic_positions, gwfn.atom_charges))
