6db18381c5c2b958a3f5ec04ef7208fcfe5be45a
Vladimir <Konjkov.VV@gmail.com>
2020-02-01 14:04:23 +0700

    ad gradient

--- gaussian.py
+++ gaussian.py
@@ -53,6 +53,44 @@ def angular_part(r, l, result, radial):
 
 
 @nb.jit(nopython=True, cache=True)
+def gradient_angular_part(r, l, result, radial):
+    """Angular part of gaussian WFN.
+    :return:
+    """
+    if l == 1:
+        result[0] += radial
+        result[1] += radial
+        result[2] += radial
+    if l == 2:
+        x, y, z = r
+        result[0] += radial * 2 * (2 * z - x - y)
+        result[1] += radial * (x + z)
+        result[2] += radial * (y + z)
+        result[3] += radial * 2 * (x - y)
+        result[4] += radial * (x + y)
+    if l == 3:
+        x, y, z = r
+        result[0] += radial * (-1.5*x**2 - 3.0*x*z - 1.5*y**2 - 3.0*y*z + 3.0*z**2)
+        result[1] += radial * (-4.5*x**2 - 3.0*x*y + 12.0*x*z - 1.5*y**2 + 6.0*z**2)
+        result[2] += radial * (-1.5*x**2 - 3.0*x*y - 4.5*y**2 + 12.0*y*z + 6.0*z**2)
+        result[3] += radial * (15.0*x**2 + 30.0*x*z - 15.0*y**2 - 30.0*y*z)
+        result[4] += radial * 30 * (x*y + x*z + y*z)
+        result[5] += radial * 45 * (x**2 - 2*x*y - y**2)
+        result[6] += radial * 45 * (x**2 + 2*x*y - y**2)
+    if l == 4:
+        x, y, z = r
+        result[0] += radial * (-7.5*x*z**2 + 1.5*x*(x**2 + y**2 + z**2) - 7.5*y*z**2 + 1.5*y*(x**2 + y**2 + z**2) + 10.0*z**3 - 6.0*z*(x**2 + y**2 + z**2))
+        result[1] += radial * (-15.0*x**2*z - 15.0*x*y*z + 37.5*x*z**2 - 7.5*x*(x**2 + y**2 + z**2) + 17.5*z**3 - 7.5*z*(x**2 + y**2 + z**2))
+        result[2] += radial * (-15.0*x*y*z - 15.0*y**2*z + 37.5*y*z**2 - 7.5*y*(x**2 + y**2 + z**2) + 17.5*z**3 - 7.5*z*(x**2 + y**2 + z**2))
+        result[3] += radial * (-30.0*x**3 + 90.0*x**2*z + 90.0*x*z**2 + 30.0*y**3 - 90.0*y**2*z - 90.0*y*z**2)
+        result[4] += radial * (-30.0*x**2*y - 30.0*x*y**2 + 180.0*x*y*z + 105.0*x*z**2 - 15.0*x*(x**2 + y**2 + z**2) + 105.0*y*z**2 - 15.0*y*(x**2 + y**2 + z**2))
+        result[5] += radial * (105.0*x**3 + 315.0*x**2*z - 315.0*x*y**2 - 630.0*x*y*z - 315.0*y**2*z)
+        result[6] += radial * (315.0*x**2*y + 315.0*x**2*z + 630.0*x*y*z - 105.0*y**3 - 315.0*y**2*z)
+        result[7] += radial * (420.0*x**3 - 1260.0*x**2*y - 1260.0*x*y**2 + 420.0*y**3)
+        result[8] += radial * (420.0*x**3 + 1260.0*x**2*y - 1260.0*x*y**2 - 420.0*y**3)
+
+
+@nb.jit(nopython=True, cache=True)
 def wfn_det(r, ne, mo, nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents):
     """Orbital coefficients for every AO at electron position r."""
     orbital = np.zeros((nbasis_functions, ne))
@@ -103,6 +141,7 @@ def gradient_det(r, ne, mo, nbasis_functions, nshell, shell_types, shell_positio
             p += primitives[shell]
             # angular part
             angular_part(rI, l, orbital[ao: ao+2*l+1, i], radial_part_1)  # 10s from 60s
+            gradient_angular_part(rI, l, orbital[ao: ao+2*l+1, i], radial_part_2)  # 10s from 60s
             ao += 2*l+1
     return np.linalg.det(np.dot(mo, orbital))   # 9s from 60s
 
@@ -147,6 +186,32 @@ def wfn(r, mo, neu, ned, nbasis_functions, nshell, shell_types, shell_positions,
         return u_orb
     d_orb = wfn_det(r[neu:], ned, mo[0][:ned], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
     return u_orb * d_orb
+
+
+def Fi(r, mo, neu, ned, nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents):
+    """Kinetic energy Fi
+    https://web.ornl.gov/~kentpr/thesis/pkthnode27.html
+    """
+    u_orb = wfn_det(r[:neu], neu, mo[0][:neu], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    u_gradient = gradient_det(r[:neu], neu, mo[0][:neu], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    if not ned:
+        return u_orb / u_gradient
+    d_orb = wfn_det(r[neu:], ned, mo[0][:ned], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    d_gradient = gradient_det(r[neu:], ned, mo[0][:ned], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    return u_orb / u_gradient + d_orb  / d_gradient
+
+
+def Ti(r, mo, neu, ned, nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents):
+    """Kinetic energy Ti
+    https://web.ornl.gov/~kentpr/thesis/pkthnode27.html
+    """
+    u_orb = wfn_det(r[:neu], neu, mo[0][:neu], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    u_laplacian = laplacian_det(r[:neu], neu, mo[0][:neu], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    if not ned:
+        return u_orb
+    d_orb = wfn_det(r[neu:], ned, mo[0][:ned], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    d_laplacian = laplacian_det(r[neu:], ned, mo[0][:ned], nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+    return u_orb / u_laplacian * d_orb / d_laplacian
 
 
 @nb.jit(nopython=True, cache=True)
@@ -229,8 +294,8 @@ def vmc(equlib, stat, mo, nshell, shell_types, shell_positions, primitives, cont
 
 @nb.jit(nopython=True, cache=True)
 def main(mo, neu, ned, nbasis_functions, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents):
-    steps = 1000 * 1000 * 10
-    offset = 3.5
+    steps = 5 * 1000 * 1000 * 1000
+    offset = 4.5
 
     x_min = np.min(shell_positions[:, 0]) - offset
     y_min = np.min(shell_positions[:, 1]) - offset
@@ -255,14 +320,14 @@ if __name__ == '__main__':
     """
     be HFcc-pVQZ
 
-    steps = 3 * 1000 * 1000 * 1000
-    offset = 3.7
+    steps = 5 * 1000 * 1000 * 1000
+    offset = 4.5
 
-    0.807685284646791
+    0.925763438273841
 
-    real    282m41,027s
-    user    282m40,507s
-    sys     0m0,452s
+    real    464m11,715s
+    user    464m11,194s
+    sys     0m0,488s
     """
 
     # gwfn = Gwfn('test/h/HF/cc-pVQZ/gwfn.data')
