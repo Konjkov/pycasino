8ca8c8f68be8df1e8cb338dca5769121103648a9
Vladimir <Konjkov.VV@gmail.com>
2020-02-22 20:11:30 +0700

    fix laplacian

--- gaussian.py
+++ gaussian.py
@@ -231,7 +231,7 @@ def numerical_gradient(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_position
             r_d[i, j] += 2 * delta
             sum += wfn(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
             r_d[i, j] -= delta
-    return sum / delta
+    return sum / delta / 2
 
 
 @nb.jit(nopython=True, cache=True)
@@ -256,7 +256,7 @@ def numerical_laplacian(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positio
             r_d[i, j] += 2 * delta
             sum += wfn(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
             r_d[i, j] -= delta
-    return sum / delta
+    return sum / delta / delta
 
 
 @nb.jit(nopython=True, cache=True)
@@ -268,7 +268,7 @@ def kinetic(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitiv
     param r: coordinat
     param mo: MO
     """
-    return numerical_laplacian(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents) / 2
+    return -numerical_laplacian(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents) / 2
 
 
 @nb.jit(nopython=True, cache=True)
@@ -278,58 +278,27 @@ def coulomb(r_u, r_d, atomic_positions, atom_charges):
     for atom in range(r_u.shape[0]):
         charge = atom_charges[atom]
         I = atomic_positions[atom]
-        x = r_u[0] - I[0]
-        y = r_u[1] - I[1]
-        z = r_u[2] - I[2]
+        x = r_u[atom][0] - I[0]
+        y = r_u[atom][1] - I[1]
+        z = r_u[atom][2] - I[2]
         r2 = x * x + y * y + z * z
         res += charge / sqrt(r2)
 
     for atom in range(r_d.shape[0]):
         charge = atom_charges[atom]
         I = atomic_positions[atom]
-        x = r_d[0] - I[0]
-        y = r_d[1] - I[1]
-        z = r_d[2] - I[2]
+        x = r_d[atom][0] - I[0]
+        y = r_d[atom][1] - I[1]
+        z = r_d[atom][2] - I[2]
         r2 = x * x + y * y + z * z
         res += charge / sqrt(r2)
 
-    return -res
+    return res
 
 
 @nb.jit(nopython=True, cache=True)
 def local_energy(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, atomic_positions, atom_charges):
     return coulomb(r_u, r_d, atomic_positions, atom_charges) + kinetic(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents) / wfn(r_u, r_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-
-
-@nb.jit(nopython=True, cache=True)
-def vmc(equlib, stat, mo, neu, ned, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, atomic_positions, atom_charges):
-
-    dX_max = 0.4
-
-    mo_u = mo[0][:neu]
-    mo_d = mo[0][:ned]
-
-    X_u = uniform(-dX_max, dX_max, (neu, 3))
-    X_d = uniform(-dX_max, dX_max, (ned, 3))
-    p = wfn(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-    for i in range(equlib):
-        new_X_u = X_u + np.random.uniform(-dX_max, dX_max, size=3)
-        new_X_d = X_d + np.random.uniform(-dX_max, dX_max, size=3)
-        new_p = wfn(new_X_u, new_X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if new_p*new_p/p/p > np.random.random_sample(1)[0]:
-            X_u, X_d, p = new_X_u, new_X_d, new_p
-
-    j = 0
-    sum = 0.0
-    for dX in range(stat):
-        new_X_u = X_u + np.random.uniform(-dX_max, dX_max, size=3)
-        new_X_d = X_d + np.random.uniform(-dX_max, dX_max, size=3)
-        new_p = wfn(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if (new_p/p)**2 > np.random.random_sample(1)[0]:
-            X_u, X_d, p = new_X_u, new_X_d, new_p
-            j += 1
-            sum += local_energy(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, atomic_positions, atom_charges)
-    return sum/j
 
 
 @nb.jit(nopython=True, cache=True)
--- vmc.py
+++ vmc.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
 
-from random import random
-
 import numpy as np
 import numba as nb
 
 from gaussian import wfn, local_energy
 from readers.gwfn import Gwfn
+from readers.input import Input
+from utils import uniform
 
 
 @nb.jit(nopython=True, cache=True)
-def vmc(equlib, stat, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, natom, atomic_positions, atom_charges):
-    dX_max = 0.5
-    X = np.random.uniform(-dX_max, dX_max, size=3)
-    p = wfn(X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+def vmc(equlib, stat, mo, neu, ned, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, atomic_positions, atom_charges):
+
+    dX_max = 0.4
+
+    mo_u = mo[0][:neu]
+    mo_d = mo[0][:ned]
+
+    X_u = uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (neu, 3))
+    X_d = uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (ned, 3))
+    p = wfn(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
     for i in range(equlib):
-        new_X = X + np.random.uniform(-dX_max, dX_max, size=3)
-        new_p = wfn(new_X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if (new_p/p)**2 > 1 or (new_p/p)**2 < random():
-            X, p = new_X, new_p
+        new_X_u = X_u + uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (neu, 3))
+        new_X_d = X_d + uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (ned, 3))
+        new_p = wfn(new_X_u, new_X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+        if new_p*new_p/p/p > np.random.random_sample(1)[0]:
+            X_u, X_d, p = new_X_u, new_X_d, new_p
 
     j = 0
     sum = 0.0
     for dX in range(stat):
-        new_X = X + np.random.uniform(-dX_max, dX_max, size=3)
-        new_p = wfn(new_X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
-        if (new_p/p)**2 > 1 or (new_p/p)**2 < random():
-            X, p = new_X, new_p
+        new_X_u = X_u + uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (neu, 3))
+        new_X_d = X_d + uniform(-np.array([dX_max, dX_max, dX_max]), np.array([dX_max, dX_max, dX_max]), (ned, 3))
+        new_p = wfn(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents)
+        if (new_p/p)**2 > np.random.random_sample(1)[0]:
+            X_u, X_d, p = new_X_u, new_X_d, new_p
             j += 1
-            sum += local_energy(X, mo, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, natom, atomic_positions, atom_charges)
+            sum += local_energy(X_u, X_d, mo_u, mo_d, nshell, shell_types, shell_positions, primitives, contraction_coefficients, exponents, atomic_positions, atom_charges)
     return sum/j
 
 
 if __name__ == '__main__':
 
     gwfn = Gwfn('test/h/HF/cc-pVQZ/gwfn.data')
-    mo = gwfn.mo[0, 0]
+    input = Input('test/h/HF/cc-pVQZ/input')
 
-    print(vmc(5000, 10**7, mo, gwfn.nshell, gwfn.shell_types, gwfn.shell_positions, gwfn.primitives, gwfn.contraction_coefficients, gwfn.exponents, gwfn.natom, gwfn.atomic_positions, gwfn.atom_charges))
+    print(vmc(5000, 10**7, gwfn.mo, input.neu, input.ned, gwfn.nshell, gwfn.shell_types, gwfn.shell_positions, gwfn.primitives, gwfn.contraction_coefficients, gwfn.exponents, gwfn.atomic_positions, gwfn.atom_charges))
