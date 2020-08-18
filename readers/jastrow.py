0f5107a0c864f3e744af604f76ab4018122501c9
Vladimir <Konjkov.VV@gmail.com>
2020-08-18 15:36:55 +0700

    fix f jastrow

--- readers/jastrow.py
+++ readers/jastrow.py
@@ -19,7 +19,7 @@ class Jastrow:
         self.u_cutoff = 0.0
         self.chi_cutoff = np.zeros(atoms.shape[0])
         self.f_cutoff = np.zeros(atoms.shape[0])
-        self.chi_cusp = False
+        self.chi_cusp = np.zeros(atoms.shape[0])
         self.jastrow = u_term = chi_term = f_term = False
         if not os.path.isfile(file):
             return
@@ -74,7 +74,9 @@ class Jastrow:
                     elif line.strip().startswith('Label'):
                         atom_labels = list(map(int, f.readline().split()))
                     elif line.strip().startswith('Impose electron-nucleus cusp'):
-                        self.chi_cusp = bool(int(f.readline()))
+                        param = bool(int(f.readline()))
+                        for atom in atom_labels:
+                            self.chi_cusp[atom-1] = param
                     elif line.strip().startswith('Expansion order'):
                         chi_order = int(f.readline())
                         chi_max_order = max(chi_max_order, chi_order)
@@ -149,64 +151,23 @@ class Jastrow:
 
         if self.chi_cutoff.any():
             self.chi_parameters = np.zeros((atoms.shape[0], chi_max_order+1, 2), np.float)
-            for i, chi_parameters in enumerate(chi_parameters_list):
-                self.chi_parameters[i, :chi_parameters.shape[0], :chi_parameters.shape[1]] = chi_parameters
-                if chi_spin_dep_list[i] == 0:
-                    self.chi_parameters[:, :, 1] = self.chi_parameters[:, :, 0]
+
             for atom in range(atoms.shape[0]):
-                self.chi_parameters[atom][1] = self.chi_parameters[atom][0] * self.trunc / self.chi_cutoff[atom]
-                if self.chi_cusp:
-                    self.chi_parameters[atom][1] -= atoms[atom]['charge'] / (-self.chi_cutoff[atom])**self.trunc
+                chi_parameters = self.fix_chi(chi_parameters_list[atom], self.chi_cutoff[atom], self.chi_cusp[atom], atoms[atom]['charge'])
+                self.chi_parameters[atom, :chi_parameters.shape[0], :chi_parameters.shape[1]] = chi_parameters
+                if chi_spin_dep_list[atom] == 0:
+                    self.chi_parameters[atom, :, 1] = self.chi_parameters[atom, :, 0]
 
         if self.f_cutoff.any():
             self.f_parameters = np.zeros((atoms.shape[0], f_en_max_order+1, f_en_max_order+1, f_ee_max_order+1, 3), np.float)
-            for i, f_parameters in enumerate(f_parameters_list):
-                self.f_parameters[i, :f_parameters.shape[0], :f_parameters.shape[1], :f_parameters.shape[2], :f_parameters.shape[3]] = f_parameters
-                if f_spin_dep_list[i] == 0:
-                    self.f_parameters[:, :, :, :, 2] = self.f_parameters[:, :, :, :, 1] = self.f_parameters[:, :, :, :, 0]
-                elif f_spin_dep_list[i] == 1:
-                    self.f_parameters[:, :, :, :, 2] = self.f_parameters[:, :, :, :, 0]
 
             for atom in range(atoms.shape[0]):
-                """fix 2 * f_en_order constrains"""
-                for lm in range(2 * f_en_order + 1):
-                    lm_sum = np.zeros(3)
-                    for l in range(f_en_order + 1):
-                        for m in range(f_en_order + 1):
-                            if l + m == lm:
-                                lm_sum += self.f_parameters[atom, l, m, 1, :]
-                    if lm < f_en_order:
-                        self.f_parameters[atom, 0, lm, 1, :] = -lm_sum / 2
-                        self.f_parameters[atom, lm, 0, 1, :] = -lm_sum / 2
-                    elif lm == f_en_order:
-                        sum_1 = -lm_sum / 2
-                    elif lm > f_en_order:
-                        self.f_parameters[atom, f_en_order, lm - f_en_order, 1, :] = -lm_sum / 2
-                        self.f_parameters[atom, lm - f_en_order, f_en_order, 1, :] = -lm_sum / 2
-
-                """fix f_en_order+f_ee_order constrains"""
-                for mn in reversed(range(f_en_order + f_ee_order + 1)):
-                    mn_sum = np.zeros(3)
-                    for m in range(f_en_order + 1):
-                        for n in range(f_ee_order + 1):
-                            if m + n == mn:
-                                mn_sum += self.trunc * self.f_parameters[atom, 0, m, n, :] - self.f_cutoff[atom] * self.f_parameters[atom, 1, m, n, :]
-                    if mn < f_en_order:
-                        self.f_parameters[atom, 0, mn, 0, :] = -mn_sum / self.trunc
-                        self.f_parameters[atom, mn, 0, 0, :] = -mn_sum / self.trunc
-                    elif mn == f_en_order:
-                        sum_2 = -mn_sum / self.trunc
-                    elif mn > f_en_order:
-                        self.f_parameters[atom, 0, f_en_order, mn - f_en_order, :] = -mn_sum / self.trunc
-                        self.f_parameters[atom, f_en_order, 0, mn - f_en_order, :] = -mn_sum / self.trunc
-
-                """fix (l=en_order - 1, m=1, n=1) term"""
-                self.f_parameters[atom, f_en_order - 1, 1, 1, :] = sum_1 - self.f_parameters[atom, f_en_order, 0, 1, :]
-                self.f_parameters[atom, 1, f_en_order - 1, 1, :] = sum_1 - self.f_parameters[atom, 0, f_en_order, 1, :]
-
-                """fix (l=en_order, m=0, n=0) term"""
-                self.f_parameters[atom, f_en_order, 0, 0, :] = sum_2 + self.f_cutoff[atom] * self.f_parameters[atom, f_en_order-1, 1, 1, :] / self.trunc
-                self.f_parameters[atom, 0, f_en_order, 0, :] = sum_2 + self.f_cutoff[atom] * self.f_parameters[atom, 1, f_en_order-1, 1, :] / self.trunc
+                f_parameters = self.fix_f(f_parameters_list[atom], self.f_cutoff[atom])
+                self.f_parameters[atom, :f_parameters.shape[0], :f_parameters.shape[1], :f_parameters.shape[2], :f_parameters.shape[3]] = f_parameters
+                if f_spin_dep_list[atom] == 0:
+                    self.f_parameters[atom, :, :, :, 2] = self.f_parameters[atom, :, :, :, 1] = self.f_parameters[atom, :, :, :, 0]
+                elif f_spin_dep_list[atom] == 1:
+                    self.f_parameters[atom, :, :, :, 2] = self.f_parameters[atom, :, :, :, 0]
 
             # self.check_f_constrains(atoms, f_en_order, f_ee_order, no_dup_u_term, no_dup_chi_term)
 
@@ -240,6 +201,58 @@ class Jastrow:
                     if no_dup_chi_term and m == 1 and n == 0:
                         mask[l, m, n] = mask[m, l, n] = False
         return mask
+
+    def fix_chi(self, chi_parameters, chi_cutoff, chi_cusp, charge):
+        chi_parameters[1] = chi_parameters[0] * self.trunc / chi_cutoff
+        if chi_cusp:
+            chi_parameters[1] -= charge / (-chi_cutoff) ** self.trunc
+        return chi_parameters
+
+    def fix_f(self, f_parameters, f_cutoff):
+        f_en_order = f_parameters.shape[0] - 1
+        f_ee_order = f_parameters.shape[2] - 1
+        f_spin_dep = f_parameters.shape[3] - 1
+        """fix 2 * f_en_order constrains"""
+        for lm in range(2 * f_en_order + 1):
+            lm_sum = np.zeros(f_spin_dep + 1)
+            for l in range(f_en_order + 1):
+                for m in range(f_en_order + 1):
+                    if l + m == lm:
+                        lm_sum += f_parameters[l, m, 1, :]
+            if lm < f_en_order:
+                f_parameters[0, lm, 1, :] = -lm_sum / 2
+                f_parameters[lm, 0, 1, :] = -lm_sum / 2
+            elif lm == f_en_order:
+                sum_1 = -lm_sum / 2
+            elif lm > f_en_order:
+                f_parameters[f_en_order, lm - f_en_order, 1, :] = -lm_sum / 2
+                f_parameters[lm - f_en_order, f_en_order, 1, :] = -lm_sum / 2
+
+        """fix f_en_order+f_ee_order constrains"""
+        for mn in reversed(range(f_en_order + f_ee_order + 1)):
+            mn_sum = np.zeros(f_spin_dep + 1)
+            for m in range(f_en_order + 1):
+                for n in range(f_ee_order + 1):
+                    if m + n == mn:
+                        mn_sum += self.trunc * f_parameters[0, m, n, :] - f_cutoff * f_parameters[1, m, n, :]
+            if mn < f_en_order:
+                f_parameters[0, mn, 0, :] = -mn_sum / self.trunc
+                f_parameters[mn, 0, 0, :] = -mn_sum / self.trunc
+            elif mn == f_en_order:
+                sum_2 = -mn_sum / self.trunc
+            elif mn > f_en_order:
+                f_parameters[0, f_en_order, mn - f_en_order, :] = -mn_sum / self.trunc
+                f_parameters[f_en_order, 0, mn - f_en_order, :] = -mn_sum / self.trunc
+
+        """fix (l=en_order - 1, m=1, n=1) term"""
+        f_parameters[f_en_order - 1, 1, 1, :] = sum_1 - f_parameters[f_en_order, 0, 1, :]
+        f_parameters[1, f_en_order - 1, 1, :] = sum_1 - f_parameters[0, f_en_order, 1, :]
+
+        """fix (l=en_order, m=0, n=0) term"""
+        f_parameters[f_en_order, 0, 0, :] = sum_2 + f_cutoff * f_parameters[f_en_order - 1, 1, 1, :] / self.trunc
+        f_parameters[0, f_en_order, 0, :] = sum_2 + f_cutoff * f_parameters[1, f_en_order - 1, 1, :] / self.trunc
+
+        return f_parameters
 
     def check_f_constrains(self, atoms, f_en_order, f_ee_order, no_dup_u_term, no_dup_chi_term):
         """"""
