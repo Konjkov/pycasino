18905ef02d9cf0332cbc1c7ceb16c073221a52f7
Vladimir <Konjkov.VV@gmail.com>
2021-03-28 15:14:51 +0700

    slater determinant in progress

--- sympy_utils/slater_derivatives.py
+++ sympy_utils/slater_derivatives.py
@@ -11,14 +11,14 @@ def gradient(momentum, n):
     for i, harmonic in enumerate(harmonics[momentum]):
         orb = harmonic * r**n * exp(-alpha*r)
         res = (
-            simplify(diff(orb, x) - (diff(harmonic, x) * r**n * exp(-alpha*r) + (n/r2 - alpha/r) * x * orb)),
-            simplify(diff(orb, y) - (diff(harmonic, y) * r**n * exp(-alpha*r) + (n/r2 - alpha/r) * y * orb)),
-            simplify(diff(orb, z) - (diff(harmonic, z) * r**n * exp(-alpha*r) + (n/r2 - alpha/r) * z * orb))
+            simplify(diff(orb, x) - (diff(harmonic, x) * r**n * exp(-alpha*r) + (n/r - alpha) * x/r * orb)),
+            simplify(diff(orb, y) - (diff(harmonic, y) * r**n * exp(-alpha*r) + (n/r - alpha) * y/r * orb)),
+            simplify(diff(orb, z) - (diff(harmonic, z) * r**n * exp(-alpha*r) + (n/r - alpha) * z/r * orb))
         )
         print("gradient({})=[{}, {}, {}]".format(i, *res))
 
 
-def hessian(momentum):
+def hessian(momentum, n):
     """
     hess(orb) =
     """
@@ -26,12 +26,12 @@ def hessian(momentum):
         l = momentum_map[momentum]
         orb = harmonic * r**n * exp(-alpha*r)
         res = (
-            diff(orb, x, x),
-            diff(orb, x, y),
-            diff(orb, y, y),
-            diff(orb, x, z),
-            diff(orb, y, z),
-            diff(orb, z, z)
+            diff(orb, x, x) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * x*x/r2 * orb,
+            diff(orb, x, y) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * x*y/r2 * orb,
+            diff(orb, y, y) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * y*y/r2 * orb,
+            diff(orb, x, z) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * x*z/r2 * orb,
+            diff(orb, y, z) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * y*z/r2 * orb,
+            diff(orb, z, z) - (alpha**2 - 2*(l+n+1)/r*alpha + (2*l+n+1)*n/r2) * z*z/r2 * orb
         )
         print("hessian({})=[{}, {}, {}, {}, {}, {}]".format(i, *res))
 
