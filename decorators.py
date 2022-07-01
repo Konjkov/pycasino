2657607a4ad119d84697015f5d232f2a461a7cf8
Vladimir <Konjkov.VV@gmail.com>
2022-07-01 17:12:44 +0700

    update requirements

--- decorators.py
+++ decorators.py
@@ -6,6 +6,23 @@ import numpy as np
 num_proc = cpu_count(logical=False)
 
 
+# def pool(function):
+#     """https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor
+#
+#     The ProcessPoolExecutor class is an Executor subclass that uses a pool of processes
+#     to execute calls asynchronously. ProcessPoolExecutor uses the multiprocessing module,
+#     which allows it to side-step the Global Interpreter Lock but also means that only
+#     picklable objects can be executed and returned.
+#     """
+#
+#     def wrapper(*args):
+#         with ProcessPoolExecutor() as executor:
+#             async_result = [executor.submit(function, *args) for _ in range(num_proc)]
+#         return np.array([res.result() for res in async_result])
+#
+#     return wrapper
+
+
 def pool(function):
     """https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor
 
@@ -17,8 +34,8 @@ def pool(function):
 
     def wrapper(*args):
         with ProcessPoolExecutor() as executor:
-            async_result = [executor.submit(function, *args) for _ in range(num_proc)]
-        return np.array([res.result() for res in async_result])
+            async_result = [executor.map(function, *[[arg] for arg in args]) for _ in range(num_proc)]
+        return np.array([res for res in async_result])
 
     return wrapper
 
