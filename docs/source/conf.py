e097119347519aeea8d061f4e3a1d6e802ecf300
Vladmir <konjkov.vv@gmail.com>
2026-05-17 14:40:31 +0700

    remove requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,8 +10,11 @@ author = 'Konkov Vladimir'
 
 extensions = ['sphinx.ext.mathjax']
 
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
